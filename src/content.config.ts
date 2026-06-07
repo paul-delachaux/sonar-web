633c4c4278a43a47fb91f971f0ee86d9595dc31d
paul-delachaux <paul.delachaux@netcourrier.com>
2026-06-07 15:56:13 +0200

    Modification de connexion à la page admin

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,65 @@
 import { defineCollection, z } from 'astro:content';
-import { glob, file } from 'astro/loaders'; // On importe glob ET file
+import { glob } from 'astro/loaders';
 
 const articles = defineCollection({
   loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/articles" }),
+
   schema: z.object({
     title: z.string(),
     description: z.string(),
     date: z.coerce.date(),
-    category: z.enum(['politique', 'culture', 'societe', 'technologie']),
+
+    category: z.enum([
+      'politique',
+      'culture',
+      'societe',
+      'technologie'
+    ]),
+
     image: z.string().optional(),
     isHero: z.boolean().default(false),
     source: z.string().optional(),
+
+    // ===== AJOUTS =====
+
+    use_thumbnail: z.boolean().optional(),
+
+    thumbnail_bg: z.string().optional(),
+    thumbnail_bg_position: z.string().optional(),
+    thumbnail_bg_zoom: z.number().optional(),
+
+    thumbnail_title: z.string().optional(),
+
+    title_x: z.number().optional(),
+    title_y: z.number().optional(),
+    title_size: z.number().optional(),
+
+    title_weight: z.union([
+      z.string(),
+      z.number()
+    ]).optional(),
+
+    title_font: z.string().optional(),
+    title_color: z.string().optional(),
+
+    thumbnail_descs: z.array(
+      z.object({
+        x: z.number().optional(),
+        y: z.number().optional(),
+        size: z.number().optional(),
+
+        weight: z.union([
+          z.string(),
+          z.number()
+        ]).optional(),
+
+        font: z.string().optional(),
+        color: z.string().optional(),
+
+        line: z.string()
+      })
+    ).optional()
   }),
 });
 
-export const collections = {articles};
+export const collections = { articles };
---
+++ src/content/articles/gecko-le-meilleur-initiateur-de-valorant.md
@@ -0,0 +1,34 @@
+---
+title: Gecko le meilleur initiateur de valorant
+description: Aujourd'hui nous parlons de ce beau gosse de Gecko
+date: 2026-05-27T14:41:00.000+02:00
+category: culture
+image: https://pbs.twimg.com/media/FuSIgc6XgAg8_1p.png
+thumbnail_bg: https://pbs.twimg.com/media/FuSIgc6XgAg8_1p.png
+thumbnail_bg_position: right
+thumbnail_bg_zoom: 50
+thumbnail_title: Gecko
+title_x: 150
+title_y: 20
+title_size: 30
+title_weight: "900"
+title_font: sans-serif
+title_color: "#ff0000"
+thumbnail_descs:
+  - x: 250
+    y: 80
+    size: 14
+    weight: "400"
+    font: sans-serif
+    color: "#ecff00"
+    line: Le meilleur initiateur
+  - x: 300
+    y: 150
+    size: 19
+    weight: "400"
+    font: sans-serif
+    color: "#0095ff"
+    line: Verti
+use_thumbnail: false
+---
+Je sais pas quoi dire sur lui en vrai sa flash pue la merde, mais mordicus j'aime bien.
