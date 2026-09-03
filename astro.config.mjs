8bd9762107a4c3845af31d1d7daf1de6169131bb
paul-delachaux <paul.delachaux@netcourrier.com>
2026-09-03 16:23:47 +0200

    Ajout du référencement du site sur google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://le-sonar.vercel.app', // Indique l'adresse de votre site pour le sitemap
   adapter: vercel(),
-  // tes autres integrations si tu en as...
+  integrations: [
+    sitemap(),
+  ],
 });
